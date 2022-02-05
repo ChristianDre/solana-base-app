13a8745b9951034aa6f7968355dae92d2ff2992e
ChristianDre <c.drechsel@arcor.de>
2022-02-05 12:00:52 +0100

    main

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,7 @@ require('@solana/wallet-adapter-react-ui/styles.css');
 import './App.css';
 
 import { Context } from './components/WalletConnection/WalletConnection';
-import { BioElement } from './components/BioElement/BioElement';;
 import {NavAppBar} from './components/Navbar/Navbar';
-
 
 export const App: FC = () => {
 
@@ -19,8 +17,10 @@ export const App: FC = () => {
     
             </Context>
             <div className="base-app-text">
-                <h1>Solana-Base-App</h1>
+                <h1>Solana-Base-App asdasd</h1>
+                <div id="canvas"></div>
             </div>
+            
             
         </div>
     );
